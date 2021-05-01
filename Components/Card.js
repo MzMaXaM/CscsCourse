1c428751257903607629344ffd5bb16cf6d90a0e
MzMaXaM <mzmaxam@gmail.com>
2021-05-01 17:50:52 +0100

    made the screens work

--- Components/Card.js
+++ Components/Card.js
@@ -1,51 +1,79 @@
-import * as React from 'react';
-import { Text, View, StyleSheet, Image, Button } from 'react-native';
-import Database from './Database'
+import React, { useState } from 'react';
+import { Text, View, StyleSheet, Image, Button, Alert } from 'react-native';
+import questions from './Database'
+const test = questions
+const n = 2
 
+const Card = () => {
+const [i, seti] = useState(0);
 
-export default function Question() {
   return (
     <View style={styles.container}>
-      <Image style={styles.pic} source={require('../assets/pexels-anamul-rezwan-1216589.jpg')} />
-      <Text style={styles.paragraph}>  Test    </Text>
-      <Text style={styles.coment}>
-       Choose one answer
-      </Text>
-
-            <input type="radio" value="no" name="option" />
-            
-            <input type="radio" value="yes" name="option" />
-            
-            <input type="radio" value="no" name="option" />
-            
-            <input type="radio" value="no" name="option" />
-            
-        <Button className="sBtn"
-          style={styles.button}
-          title="Submit"
-          />
+      <Image style={styles.pic} source={test[i].pic} />
+      <Text style={styles.question}>{test[i].question}</Text>
+      <Text style={styles.coment}>Choose one answer</Text>
+      <View style={styles.questionsContainer}>
+      <View style={styles.radioOption}>
+        <input type="radio"  name="option" />
+        <Text>{test[i].a} </Text>
+      </View>
+      <View style={styles.radioOption}>
+        <input type="radio" value="yes" name="option" />
+        <Text>{test[i].b} </Text>
+      </View>
+      <View style={styles.radioOption}>
+        <input type="radio"  name="option" />
+        <Text>{test[i].c} </Text>
+      </View>
+      <View style={styles.radioOption}>
+        <input type="radio"  name="option" />
+        <Text>{test[i].d} </Text>
+      </View>
+      </View>
+      <View style={styles.buttonPlace}>
+      <Button 
+        style={styles.button}
+        title="Submit"
+        onPress={() => seti(i + 1)}/>
+      </View>
+      <View style={styles.logBox}>
+        <Text>clicked {i} times </Text>
+      </View>
     </View>
-  );
-}
+  )};
 
 const styles = StyleSheet.create({
     container: {
+      width: 400,
+      padding: 10,
+      backgroundColor: 'white',
+      borderRadius: 10,
       alignItems: 'center',
-      justifyContent: 'center',
-      padding: 24,
+      justifyContent: 'center'
     },
-    paragraph: {
-      marginTop: 20,
+    questionsContainer: { },
+    question: {
+      marginTop: 15,
       fontSize: 14,
       fontWeight: 'bold',
       textAlign: 'center',
     },
     pic: {
       height: 165,
-      width: 165,
+      width: 185,
+      borderRadius:5
     },
     coment:{
+      marginVertical: 5,
       fontSize: 11,
       color: 'orange',
+    },
+    radioOption: {
+      flexDirection: 'row'
+    },
+    buttonPlace: {
+      marginTop: 10
     }
   });
+
+export default Card;
--- Components/Database.js
+++ Components/Database.js
@@ -1,29 +1,31 @@
-import React, {useState} from 'react';
-import { Text, View, StyleSheet, Image, Button } from 'react-native';
-
-
-
-export default function Question() {
-const [questions, setQuestions] = useState([
- {
-    key:1,
-    question: 'Which of these are not legal duty as a worker?',
-    a: 'Not to interfere with anything provided for health and safety',
-    b: 'To look after the health and safety of anyone else who might be affected by your work',
-    c: 'To look after your own health and safety',
-    d: 'To write your own risk assessments',
-    ca: 'd',
-    pic:   <Image style={styles.pic} source={require('../assets/pexels-anamul-rezwan-1216589.jpg')} />
- },
-{
-    key: 2,
-    question: 'What does a risk assessment identify?',
-    a: 'How to report accidents',
-    b: 'The hazards and safe way of doing the job',
-    c: 'The site working hours',
-    d: 'Where the first-aid box is kept',
-    ca: 'b',
-    pic:   <Image style={styles.pic} source={require('../assets/pexels-anamul-rezwan-1216589.jpg')} />
+const questions = {
+    0: {
+        question: 'Which of these are not legal duty as a worker?',
+        a: 'Not to interfere with anything provided for health and safety',
+        b: 'To look after the health and safety of anyone else who might be affected by your work',
+        c: 'To look after your own health and safety',
+        d: 'To write your own risk assessments',
+        ca: 'd',
+        pic: require('../assets/pexels-anamul-rezwan-1216589.jpg')
+    },
+    1: {
+        question: 'What does a risk assessment identify?',
+        a: 'How to report accidents',
+        b: 'The hazards and safe way of doing the job',
+        c: 'The site working hours',
+        d: 'Where the first-aid box is kept',
+        ca: 'b',
+        pic: require('../assets/pexels-anamul-rezwan-1216589.jpg')
+    },
+    2: {
+        question: 'What does the word "Hazard" mean',
+        a: 'A type of barrier or machine guard',
+        b: 'Anything at work that could harm you',
+        c: 'The likelihood that something might happen',
+        d: 'The site accident rate',
+        ca: 'b',
+        pic: require('../assets/pexels-pixabay-159306.jpg')
+    }
 }
-]);
-}
+
+export default questions;
