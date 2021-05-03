9944153e2ee09acddd4c3c5e7b79606d5d4d4606
MzMaXaM <mzmaxam@gmail.com>
2021-05-03 13:24:56 +0100

    updated radioButtons

--- Components/Card.js
+++ Components/Card.js
@@ -1,51 +1,46 @@
 import React, { useState } from 'react';
-import { Text, View, StyleSheet, Image, Button, TouchableOpacity } from 'react-native';
-import questions from './Database'
-
-const qst = questions
-let rsp = []
+import { View, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { RadioButton, Text, Button } from 'react-native-paper';
+import qst from './Database'
 
 const Card = () => {
-const [i, seti] = useState(0);
+  const [value, setValue] = useState(' ');
+  const [i, seti] = useState(0);
 
   return (
     <View style={styles.container}>
       <Image style={styles.pic} source={qst[i].pic} />
       <Text style={styles.question}>{qst[i].question}</Text>
-      <Text style={styles.coment}>Choose one answer</Text>
-      {/* <View style={styles.questionsContainer}>
-        <TouchableOpacity onPress={() => this.handleChange} >
-          <View style={styles.radioOption}>
-            <input type="radio"  name="option" />
-            <Text>{qst[i].a} </Text>
-          </View></TouchableOpacity>  
-        <TouchableOpacity>
-          <View style={styles.radioOption}>
-            <input type="radio" value="yes" name="option" />
-            <Text>{qst[i].b} </Text>
-          </View></TouchableOpacity>
-        <TouchableOpacity>
-          <View style={styles.radioOption}>
-            <input type="radio"  name="option" />
-            <Text>{qst[i].c} </Text>
-          </View></TouchableOpacity>
-        <TouchableOpacity>
-          <View style={styles.radioOption}>
-            <input type="radio"  name="option" />
-            <Text>{qst[i].d} </Text>
-          </View></TouchableOpacity>
-      </View> */}
-      <View style={styles.buttonPlace}>
-      <Button 
-        style={styles.button}
-        title="Submit"
-        onPress={() => (seti(i + 1),
-        rsp.push(1)
-        )}/>
-      </View>
-      <View style={styles.logBox}>
-        <Text>clicked {i} times </Text>
-      </View>
+    <RadioButton.Group onValueChange={newValue => setValue(newValue)} value={value}>
+        <TouchableOpacity onPress={() =>  setValue('a')}> 
+            <View style={styles.radioContainer}>
+                <RadioButton value="a" />
+                <Text style={styles.radioText}>{qst[i].a}</Text>
+            </View>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() =>  setValue('b')}>
+            <View style={styles.radioContainer}>
+                <RadioButton value="b" />
+                <Text style={styles.radioText}>{qst[i].b}</Text>
+            </View>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() =>  setValue('c')}> 
+            <View style={styles.radioContainer}>
+                <RadioButton value="c" />
+                <Text style={styles.radioText}>{qst[i].c}</Text>
+            </View>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() =>  setValue('d')}>
+            <View style={styles.radioContainer}>
+                <RadioButton value="d" />
+                <Text style={styles.radioText}>{qst[i].d}</Text>
+            </View>
+        </TouchableOpacity>
+        
+    </RadioButton.Group>
+    <Button icon="camera" mode="contained" onPress={() => seti(i + 1)}>
+     Check Answer
+    </Button>
     </View>
   )};
 
@@ -80,6 +75,14 @@ const styles = StyleSheet.create({
     },
     buttonPlace: {
       marginTop: 10
+    },
+    radioContainer: {
+      width: 400,
+      flexDirection: 'row'
+    },
+    radioText:{
+      marginVertical: 5,
+      fontSize: 14
     }
   });
 
--- Components/mockTest.js
+++ Components/mockTest.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 import Card from './Card';
-import RadioBtn from './RadioButtons.js'
 
 let rsp = []
 
 function MockTest() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <RadioBtn/>
+        <Card/>
       </View>
     );
   }
